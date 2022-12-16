4824abcdb73fab59edf88ffbb676fdf4a2716f0c
Edward Morris <teggy1@hotmail.com>
2022-12-16 15:13:20 +0100

    can't find my to do list

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text, onClick}) => {
-    
-  return <button onClick={onClick}
+const Button = ({color,text, onClick}) => {
+
+  return (
+    <button 
+        onClick={onClick} 
         style={{backgroundColor: color}} 
-        className = 'btn'>
-            {text}</button>
-  
+        className='btn'>
+            {text}
+        </button>
+  )
 }
 
-Button.propTypes = {
+Button.defaultProps = {
     color: 'steelblue'
 }
 
